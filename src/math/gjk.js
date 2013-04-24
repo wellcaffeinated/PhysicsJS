c9c4b4a3833e8585baf490a9e22d3a589be1cc26
Jasper <wellcaffeinated@iCoffee.local>
2013-04-24 03:08:56 -0400

    work to modify gjk to get distance also

--- src/math/gjk.js
+++ src/math/gjk.js
@@ -4,6 +4,33 @@
 // * http://mollyrocket.com/849
 (function(){
 
+var gjkAccuracy = 0.000001;
+
+// get the next search direction from two simplex points
+var getNextSearchDir = function getNextSearchDir( ptA, ptB, dir ){
+
+    var ABdotB = ptB.normSq() - ptB.dot( ptA )
+        ,ABdotA = ptB.dot( ptA ) - ptA.normSq()
+        ;
+
+    if ( ABdotB < 0 ){
+
+        return dir.clone( ptB ).negate();
+
+    } else if ( ABdotA > 0 ){
+
+        return dir.clone( ptA ).negate();
+
+    } else {
+
+        // dir = AB = B - A
+        dir.clone( ptB ).vsub( ptA );
+        // if (left handed coordinate system) 
+        // A cross AB < 0 then get perpendicular counter clockwise 
+        return dir.perp( (ptA.cross( dir ) < 0) );
+    }
+};
+
 /**
  * Implementation agnostic GJK function.
  * @param  {Function} support The support function. 
@@ -12,9 +39,11 @@
  * @param {Physics.vector} seed The starting direction for the simplex
  * @return {Object} The algorithm information containing properties: .overlap (bool), and .simplex (Array)
  */
-var gjk = function gjk( support, seed ){
+var gjk = function gjk( support, seed, checkOverlapOnly ){
 
     var overlap = false
+        ,noOverlap = false // if we're sure we're not overlapping
+        ,distance = false
         ,simplex = []
         ,simplexLen = 1
         // setup a scratchpad of temporary cheap objects
@@ -27,6 +56,7 @@ var gjk = function gjk( support, seed ){
         ,ac
         ,sign
         ,tmp
+        ,iterations = 0
         ;
 
     // get the first Minkowski Difference point
@@ -34,106 +64,158 @@ var gjk = function gjk( support, seed ){
     simplexLen = simplex.push( last );
     // negate d for the next point
     dir.negate();
+
     // start looping
-    while ( true ) {
+    while ( ++iterations ) {
+
         // push a new point to the simplex because we haven't terminated yet
         lastlast = last;
         last = support( dir );
         simplexLen = simplex.push( last );
 
-        // check if the last point we added actually passed the origin
-        if ( last.dot( dir ) <= 0.0 && !last.equals(Physics.vector.zero) ) {
-            // if the point added last was not past the origin in the direction of d
-            // then the Minkowski Sum cannot possibly contain the origin since
-            // the last point added is on the edge of the Minkowski Difference
+        if ( last.equals(Physics.vector.zero) ){
+            // we happened to pick the origin as a support point... lucky.
+            overlap = true;
             break;
-        } else {
+        }
+        
+        // check if the last point we added actually passed the origin
+        if ( !noOverlap && last.dot( dir ) <= 0.0 ) {
+            // if the point added last was not past the origin in the direction of d
+            // then the Minkowski difference cannot possibly contain the origin since
+            // the last point added is on the edge of the Minkowski Difference
+
+            // if we just need the overlap...
+            if ( checkOverlapOnly ){
+                break;
+            }
+
+            noOverlap = true;
+
+        // if it's a line...
+        } else if ( simplexLen === 2 ){
 
             // otherwise we need to determine if the origin is in
             // the current simplex and act accordingly
 
-            // if it's a line...
-            if ( simplexLen === 2 ){
-                // dir = AB = B - A
-                dir.clone( lastlast ).vsub( last );
-                // if (left handed coordinate system) 
-                // A cross AB < 0 then get perpendicular counter clockwise 
-                dir.perp( (last.cross( dir ) < 0) );
+            dir = getNextSearchDir( last, lastlast, dir );
+            // continue...
 
-            // if it's a triangle...
-            } else if ( simplexLen === 3 ){
+        // if it's a triangle... and we're looking for the distance
+        } else if ( noOverlap ){
 
-                ab = ab || scratch.vector();
-                ac = ac || scratch.vector();
+            // if we know there isn't any overlap and
+            // we're just trying to find the distance...
+            // make sure we're getting closer to the origin
+            dir.normalize();
+            tmp = last.dot( dir );
+            if ( (tmp - lastlast.dot( dir )) < gjkAccuracy ){
 
-                // get the edges AB and AC
-                ab.clone( lastlast ).vsub( last );
-                ac.clone( simplex[ 0 ] ).vsub( last );
+                distance = -tmp;
+                break;
+            }
 
-                // here normally people think about this as getting outward facing
-                // normals and checking dot products. Since we're in 2D
-                // we can be clever...
-                sign = ab.cross( ac ) > 0;
-                tmp = last.cross( ab );
-                // TODO: if tmp === 0, then the origin is ON an edge... optimization?
+            // if we are still getting closer then only keep
+            // the points in the simplex that are closest to
+            // the origin (we already know that last is closer
+            // than the previous two)
+            // the norm is the same as distance(origin, a)
+            // use norm squared to avoid the sqrt operations
+            if (lastlast.normSq() < simplex[ 0 ].normSq()) {
+                
+                simplex.shift();
 
-                if ( sign ^ (tmp > 0) ){
+            } else {
+                
+                simplex.splice(1, 1);
+            }
 
-                    // ok... so there's an XOR here... don't freak out
-                    // remember last = A = -AO
-                    // if AB cross AC and AO cross AB have the same sign
-                    // then the origin is along the outward facing normal of AB
-                    // so if AB cross AC and A cross AB have _different_ (XOR) signs
-                    // then this is also the case... so we proceed...
+            dir = getNextSearchDir( simplex[ 1 ], simplex[ 0 ], dir );
+            // continue...
 
-                    // morph the ab vector into its outward facing normal
-                    ab.perp( sign );
-                    // swap names
-                    sign = dir;
-                    dir = ab;
-                    ab = sign;
-                    // point C is dead to us now...
-                    simplex.shift();
-                    // continue...
+        // if it's a triangle
+        } else {
 
-                } else {
-                    // this means we can continue to look along
-                    // the other outward normal direction (ACperp)
-                    // if we don't see the origin... then we must have it enclosed
+            // we need to trim the useless point...
 
-                    tmp = ac.cross( last );
-                    if ( sign ^ (tmp > 0) ){
-                        // then the origin is along the outward facing normal 
-                        // of AC; (ACperp)
+            ab = ab || scratch.vector();
+            ac = ac || scratch.vector();
 
-                        ac.perp( !sign );
-                        // swap names
-                        sign = dir;
-                        dir = ac;
-                        ac = sign;
-                        // point B is dead to us now...
-                        simplex.splice(1, 1);
-                        // continue...
+            // get the edges AB and AC
+            ab.clone( lastlast ).vsub( last );
+            ac.clone( simplex[ 0 ] ).vsub( last );
 
-                    } else {
+            // here normally people think about this as getting outward facing
+            // normals and checking dot products. Since we're in 2D
+            // we can be clever...
+            sign = ab.cross( ac ) > 0;
+            
+            if ( sign ^ (last.cross( ab ) > 0) ){
 
-                        // we have enclosed the origin!
-                        overlap = true;
-                        // take a break...
-                        break;
-                    }
-                }
+                // ok... so there's an XOR here... don't freak out
+                // remember last = A = -AO
+                // if AB cross AC and AO cross AB have the same sign
+                // then the origin is along the outward facing normal of AB
+                // so if AB cross AC and A cross AB have _different_ (XOR) signs
+                // then this is also the case... so we proceed...
+
+                // point C is dead to us now...
+                simplex.shift();
+
+                // if we haven't deduced that we've enclosed the origin
+                // then we know which way to look...
+                // morph the ab vector into its outward facing normal
+                ab.perp( sign );
+                // swap names
+                sign = dir;
+                dir = ab;
+                ab = sign;
+                
+                // continue...
+
+                // if we get to this if, then it means we can continue to look along
+                // the other outward normal direction (ACperp)
+                // if we don't see the origin... then we must have it enclosed
+            } else if ( sign ^ (ac.cross( last ) > 0) ){
+                // then the origin is along the outward facing normal 
+                // of AC; (ACperp)
+
+                // point B is dead to us now...
+                simplex.splice(1, 1);
+
+                ac.perp( !sign );
+                // swap names
+                sign = dir;
+                dir = ac;
+                ac = sign;
+                
+                // continue...
+
+            } else {
+
+                // we have enclosed the origin!
+                overlap = true;
+                // fewf... take a break
+                break;
             }
         }
     }
 
+    console.log(iterations)
     // free workspace
     scratch.done();
 
-    return {
+    tmp = {
         overlap: overlap,
         simplex: simplex
+    };
+
+    if ( distance !== false ){
+
+        tmp.distance = distance;
     }
+
+    return tmp;
 };
 
 Physics.gjk = gjk;
