f7f22e57a9a0141063fefa41744ffccbb4c8b6b4
Jasper <wellcaffeinated@iCoffee.local>
2013-11-19 14:25:19 -0500

    refactor ship demo

---
+++ examples/spaceship/js/player-behavior.js
@@ -0,0 +1,121 @@
+define(
+    [
+        'physicsjs'
+    ],
+    function(
+        Physics
+    ){
+
+        return Physics.behavior('player-behavior', function( parent ){
+
+            return {
+                init: function( options ){
+                    var self = this;
+                    parent.init.call(this, options);
+                    // store the player
+                    var player = self.player = options.player;
+                    self.gameover = false;
+
+                    // events
+                    document.addEventListener('keydown', function( e ){
+                        if (self.gameover){
+                            return;
+                        }
+                        switch ( e.keyCode ){
+                            case 38: // up
+                                self.movePlayer();
+                            break;
+                            case 40: // down
+                            break;
+                            case 37: // left
+                                player.turn( -1 );
+                            break;
+                            case 39: // right
+                                player.turn( 1 );
+                            break;
+                            case 32: // space
+                                player.shoot();
+                            break;
+                        }
+                        return false;
+                    });
+                    document.addEventListener('keyup', function( e ){
+                        if (self.gameover){
+                            return;
+                        }
+                        switch ( e.keyCode ){
+                            case 38: // up
+                                self.movePlayer( false );
+                            break;
+                            case 40: // down
+                            break;
+                            case 37: // left
+                                player.turn( 0 );
+                            break;
+                            case 39: // right
+                                player.turn( 0 );
+                            break;
+                            case 32: // space
+                            break;
+                        }
+                        return false;
+                    });
+                },
+
+                connect: function( world ){
+                    world.subscribe('collisions:detected', this.checkPlayerCollision, this);
+                    world.subscribe('integrate:positions', this.behave, this);
+                },
+
+                disconnect: function( world ){
+                    world.unsubscribe('collisions:detected', this.checkPlayerCollision);
+                    world.unsubscribe('integrate:positions', this.behave);
+                },
+
+                // check to see if the player has collided
+                checkPlayerCollision: function( data ){
+                    
+                    var self = this
+                        ,world = self._world
+                        ,collisions = data.collisions
+                        ,col
+                        ,player = this.player
+                        ;
+
+                    for ( var i = 0, l = collisions.length; i < l; ++i ){
+                        col = collisions[ i ];
+
+                        // if we aren't looking at debris
+                        // and one of these bodies is the player...
+                        if ( col.bodyA.gameType !== 'debris' && 
+                            col.bodyB.gameType !== 'debris' && 
+                            (col.bodyA === player || col.bodyB === player) 
+                        ){
+                            player.blowUp();
+                            world.removeBehavior( this );
+                            this.gameover = true;
+                            world.publish('gameover');
+                            return;
+                        }
+                    }
+                },
+
+                // toggle player motion
+                movePlayer: function( active ){
+
+                    if ( active === false ){
+                        this.playerMove = false;
+                        return;
+                    }
+                    this.playerMove = true;
+                },
+
+                behave: function( data ){
+
+                    // activate thrusters if playerMove is true
+                    this.player.thrust( this.playerMove ? 1 : 0 );
+                }
+            };
+        });
+    }
+);
---
+++ examples/spaceship/js/space.js
@@ -0,0 +1,188 @@
+define([
+    'require',
+    'physicsjs',
+
+    // custom modules
+    './player',
+    './player-behavior',
+    './asteroid',
+
+    // official modules
+    'physicsjs/renderers/canvas',
+    'physicsjs/bodies/circle',
+    'physicsjs/bodies/convex-polygon',
+    'physicsjs/behaviors/newtonian',
+    'physicsjs/behaviors/sweep-prune',
+    'physicsjs/behaviors/body-collision-detection',
+    'physicsjs/behaviors/body-impulse-response'
+], function(
+    require,
+    Physics
+){
+    document.body.className = 'before-game';
+    var inGame = false;
+    document.addEventListener('keydown', function( e ){
+
+        if (!inGame && e.keyCode === 32){
+            document.body.className = 'in-game';
+            inGame = true;
+            newGame();
+        }
+    });
+
+    var renderer = Physics.renderer('canvas', {
+        el: 'viewport',
+        width: window.innerWidth,
+        height: window.innerHeight,
+        // meta: true,
+        // debug:true,
+        styles: {
+            'circle': {
+                strokeStyle: 'hsla(0, 0%, 30%, 1)',
+                lineWidth: 1,
+                fillStyle: 'hsla(0, 0%, 30%, 1)',
+                angleIndicator: false
+            },
+            'convex-polygon' : {
+                strokeStyle: 'rgb(0, 0, 0)',
+                lineWidth: 1,
+                fillStyle: 'rgb(0, 16, 11)',
+                angleIndicator: false
+            }
+        }
+    });
+
+    var world = null;
+    var newGame = function newGame(){
+
+        if (world){
+            world.destroy();
+        }
+
+        world = Physics( init );
+        world.subscribe('game-over', function(){
+            document.body.className = 'game-over';
+            inGame = false;
+        });      
+    };
+
+    // subscribe to ticker and start looping
+    Physics.util.ticker.subscribe(function( time ){
+        if (world){
+            world.step( time ); 
+        }
+    }).start();
+
+    var init = function init( world, Physics ){
+
+        // bodies
+        var ship = Physics.body('player', {
+            x: 400,
+            y: 100,
+            vx: 0.08,
+            radius: 30
+        });
+
+        var playerBehavior = Physics.behavior('player-behavior', { player: ship });
+        
+        for ( var i = 0, l = 30; i < l; ++i ){
+
+            var ang = 4 * (Math.random() - 0.5) * Math.PI;
+            var r = 700 + 100 * Math.random() + i * 10;
+
+            world.add( Physics.body('asteroid', {
+                x: 400 + Math.cos( ang ) * r,
+                y: 300 + Math.sin( ang ) * r,
+                vx: 0.03 * Math.sin( ang ),
+                vy: - 0.03 * Math.cos( ang ),
+                angularVelocity: (Math.random() - 0.5) * 0.001,
+                radius: 50,
+                mass: 30,
+                restitution: 0.6
+            }));
+        }
+
+        var planet = Physics.body('circle', {
+            // fixed: true,
+            // hidden: true,
+            mass: 10000,
+            radius: 120,
+            x: 400,
+            y: 300
+        });
+        planet.view = new Image();
+        planet.view.src = require.toUrl('/images/planet.png');
+
+        // middle of canvas
+        var middle = { 
+            x: 0.5 * renderer.options.width, 
+            y: 0.5 * renderer.options.height
+        };
+        // render on every step
+        world.subscribe('step', function(){
+            // follow player
+            renderer.options.offset.clone( middle ).vsub( ship.state.pos );
+            world.render();
+        });
+
+        // blow up anything that touches a laser pulse
+        world.subscribe('collisions:detected', function( data ){
+            var collisions = data.collisions
+                ,col
+                ;
+
+            for ( var i = 0, l = collisions.length; i < l; ++i ){
+                col = collisions[ i ];
+
+                if ( col.bodyA.gameType === 'laser' || col.bodyB.gameType === 'laser' ){
+                    if ( col.bodyA.blowUp ){
+                        col.bodyA.blowUp();
+                        world.removeBody( col.bodyB );
+                    } else if ( col.bodyB.blowUp ){
+                        col.bodyB.blowUp();
+                        world.removeBody( col.bodyA );
+                    }
+                    return;
+                }
+            }
+        });
+
+        // draw minimap
+        world.subscribe('render', function( data ){
+            var r = 100;
+            var shim = 15;
+            var x = renderer.options.width - r - shim;
+            var y = r + shim;
+            var scratch = Physics.scratchpad();
+            var d = scratch.vector();
+            var lightness;
+
+            renderer.drawCircle(x, y, r, { strokeStyle: '#090', fillStyle: '#010' });
+            renderer.drawCircle(x, y, r * 2 / 3, { strokeStyle: '#090' });
+            renderer.drawCircle(x, y, r / 3, { strokeStyle: '#090' });
+
+            for (var i = 0, l = data.bodies.length, b = data.bodies[ i ]; b = data.bodies[ i ]; i++){
+
+                d.clone( ship.state.pos ).vsub( b.state.pos ).mult( -0.05 );
+                lightness = Math.max(Math.min(Math.sqrt(b.mass*10)|0, 100), 10);
+                if (d.norm() < r){
+                    renderer.drawCircle(x + d.get(0), y + d.get(1), 1, 'hsl(60, 100%, '+lightness+'%)');
+                }
+            }
+
+            scratch.done();
+        });
+
+        // add things to the world
+        world.add([
+            ship,
+            playerBehavior,
+            planet,
+            Physics.behavior('newtonian', { strength: 1e-4 }),
+            Physics.behavior('sweep-prune'),
+            Physics.behavior('body-collision-detection'),
+            Physics.behavior('body-impulse-response'),
+            renderer
+        ]);
+    };
+});
