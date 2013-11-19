ab22d390ce9719968f9b75298c57b25fb5fa3c9c
Jasper <wellcaffeinated@iCoffee.local>
2013-11-19 01:28:32 -0500

    added projectiles

--- examples/spaceship/space.js
+++ examples/spaceship/space.js
@@ -2,8 +2,8 @@ Physics(function( world, Physics ){
 
     var renderer = Physics.renderer('canvas', {
         el: 'viewport',
-        width: document.width,
-        height: document.height,
+        width: window.innerWidth,
+        height: window.innerHeight,
         meta: true,
         // debug:true,
         styles: {
@@ -32,6 +32,36 @@ Physics(function( world, Physics ){
         shipImg.src = './ship.png';
         shipThrustImg.src = './ship-thrust.png';
 
+        var Pi2 = 2 * Math.PI;
+        // VERY crude approximation to a gaussian random number.. but fast
+        var gauss = function gauss( mean, stddev ){
+            var r = 2 * (Math.random() + Math.random() + Math.random()) - 3;
+            return r * stddev + mean;
+        };
+        // will give a random polygon that, for small jitter, will likely be convex
+        var rndPolygon = function rndPolygon( size, n, jitter ){
+
+            var points = [{ x: 0, y: 0 }]
+                ,ang = 0
+                ,invN = 1 / n
+                ,mean = Pi2 * invN
+                ,stddev = jitter * (invN - 1/(n+1)) * Pi2
+                ,i = 1
+                ,last = points[ 0 ]
+                ;
+
+            while ( i < n ){
+                ang += gauss( mean, stddev );
+                points.push({
+                    x: size * Math.cos( ang ) + last.x,
+                    y: size * Math.sin( ang ) + last.y
+                });
+                last = points[ i++ ];
+            }
+
+            return points;
+        };
+
         return {
             init: function( options ){
                 parent.init.call( this, options );
@@ -44,7 +74,7 @@ Physics(function( world, Physics ){
             thrust: function( amount ){
                 var angle = this.state.angular.pos;
                 var scratch = Physics.scratchpad();
-                amount *= 0.0001;
+                amount *= 0.00001;
                 var v = scratch.vector().set(
                     amount * Math.cos( angle ), 
                     amount * Math.sin( angle ) 
@@ -57,7 +87,60 @@ Physics(function( world, Physics ){
                 } else {
                     this.view = shipImg;
                 }
-            }
+            },
+            shoot: function(){
+                var angle = this.state.angular.pos;
+                var cos = Math.cos( angle );
+                var sin = Math.sin( angle );
+                var r = this.geometry.radius + 5;
+                var laser = Physics.body('circle', {
+                    x: this.state.pos.get(0) + r * cos,
+                    y: this.state.pos.get(1) + r * sin,
+                    vx: (0.5 + this.state.vel.get(0)) * cos,
+                    vy: (0.5 + this.state.vel.get(1)) * sin,
+                    radius: 2
+                });
+                laser.gameType = 'laser';
+
+                setTimeout(function(){
+                    world.removeBody( laser );
+                    laser = undefined;
+                }, 600);
+                world.add( laser );
+            },
+            blowUp: function(){
+                var scratch = Physics.scratchpad();
+                var rnd = scratch.vector();
+                var pos = this.state.pos;
+                var n = 40;
+                var r = 2 * this.geometry.radius;
+                var size = 8 * r / n;
+                var mass = this.mass / n;
+                var verts;
+                var debris = [];
+                
+                // create particles
+                while ( n-- ){
+                    verts = rndPolygon( size, 3, 1.5 );
+                    if ( Physics.geometry.isPolygonConvex( verts ) ){
+                        rnd.set( Math.random() - 0.5, Math.random() - 0.5 ).mult( r );
+                        debris.push( Physics.body('convex-polygon', {
+                            x: pos.get(0) + rnd.get(0),
+                            y: pos.get(1) + rnd.get(1),
+                            vx: this.state.vel.get(0),
+                            vy: this.state.vel.get(1),
+                            angularVelocity: (Math.random()-0.5) * 0.06,
+                            mass: mass,
+                            vertices: verts,
+                            restitution: 0.8
+                        }));
+                    }
+                }
+
+                world.add( debris );
+                world.removeBody( this );
+                scratch.done();
+            },
         };
     }, {
         x: 400,
@@ -66,14 +149,41 @@ Physics(function( world, Physics ){
         radius: 30
     });
 
+    world.subscribe('collisions:detected', function( data ){
+        var collisions = data.collisions
+            ,col
+            ;
+
+        for ( var i = 0, l = collisions.length; i < l; ++i ){
+            col = collisions[ i ];
+
+            if ( col.bodyA.gameType === 'laser' || col.bodyB.gameType === 'laser' ){
+                if ( col.bodyA.blowUp ){
+                    col.bodyA.blowUp();
+                    world.removeBody( col.bodyB );
+                } else if ( col.bodyB.blowUp ){
+                    col.bodyB.blowUp();
+                    world.removeBody( col.bodyA );
+                }
+                return;
+            }
+        }
+    });
+
     var playerBehavior = Physics.behavior('player', function( parent ){
+
         return {
             init: function( options ){
                 var self = this;
                 parent.init.call(this, options);
+                var player = self.player = options.player;
+                self.gameover = false;
 
                 // events
                 document.addEventListener('keydown', function( e ){
+                    if (self.gameover){
+                        return;
+                    }
                     switch ( e.keyCode ){
                         case 38: // up
                             self.movePlayer();
@@ -81,14 +191,20 @@ Physics(function( world, Physics ){
                         case 40: // down
                         break;
                         case 37: // left
-                            ship.turn( -1 );
+                            player.turn( -1 );
                         break;
                         case 39: // right
-                            ship.turn( 1 );
+                            player.turn( 1 );
+                        break;
+                        case 32: // space
+                            player.shoot();
                         break;
                     }
                 });
                 document.addEventListener('keyup', function( e ){
+                    if (self.gameover){
+                        return;
+                    }
                     switch ( e.keyCode ){
                         case 38: // up
                             self.movePlayer( false );
@@ -96,10 +212,12 @@ Physics(function( world, Physics ){
                         case 40: // down
                         break;
                         case 37: // left
-                            ship.turn( 0 );
+                            player.turn( 0 );
                         break;
                         case 39: // right
-                            ship.turn( 0 );
+                            player.turn( 0 );
+                        break;
+                        case 32: // space
                         break;
                     }
                 });
@@ -118,50 +236,19 @@ Physics(function( world, Physics ){
             checkPlayerCollision: function( data ){
                 var collisions = data.collisions
                     ,col
+                    ,player = this.player
                     ;
 
                 for ( var i = 0, l = collisions.length; i < l; ++i ){
                     col = collisions[ i ];
 
-                    if ( col.bodyA === ship || col.bodyB === ship ){
-                        this.destroyPlayer();
+                    if ( col.bodyA.gameType !== 'debris' && col.bodyB.gameType !== 'debris' && (col.bodyA === player || col.bodyB === player) ){
+                        player.blowUp();
+                        world.removeBehavior( this );
+                        this.gameover = true;
                         return;
                     }
                 }
-            },
-
-            destroyPlayer: function(){
-                var scratch = Physics.scratchpad();
-                var rnd = scratch.vector();
-                var pos = ship.state.pos;
-                var n = 40;
-                var r = 2 * ship.geometry.radius;
-                var size = 4 * r / n;
-                var mass = ship.mass / n;
-                var verts = [
-                    { x: 0, y: 0 },
-                    { x: 0, y: size },
-                    { x: size, y: size },
-                    { x: size, y: 0 }
-                ];
-
-                // create particles
-                while ( n-- ){
-                    rnd.set( Math.random() - 0.5, Math.random() - 0.5 ).mult( r );
-                    world.add( Physics.body('convex-polygon', {
-                        x: pos.get(0) + rnd.get(0),
-                        y: pos.get(1) + rnd.get(1),
-                        vx: ship.state.vel.get(0),
-                        vy: ship.state.vel.get(1),
-                        angularVelocity: (Math.random()-0.5) * 0.06,
-                        mass: mass,
-                        vertices: verts,
-                        restitution: 0.8
-                    }));
-                }
-
-                world.removeBody( ship );
-                scratch.done();
             },
 
             movePlayer: function( active ){
@@ -175,10 +262,10 @@ Physics(function( world, Physics ){
 
             behave: function( data ){
 
-                ship.thrust( this.playerMove ? 1 : 0 );
+                this.player.thrust( this.playerMove ? 1 : 0 );
             }
         };
-    }, {});
+    }, { player: ship });
     
     Physics.body('asteroid', 'circle', function( parent ){
         var ast1 = new Image();
@@ -189,11 +276,51 @@ Physics(function( world, Physics ){
                 parent.init.call(this, options);
 
                 this.view = ast1;
+            },
+            blowUp: function(){
+                var scratch = Physics.scratchpad();
+                var rnd = scratch.vector();
+                var pos = this.state.pos;
+                var n = 40;
+                var r = 2 * this.geometry.radius;
+                var size = r / n;
+                var mass = 0.001;
+                var d;
+                var debris = [];
+                
+                // create particles
+                while ( n-- ){
+                    rnd.set( Math.random() - 0.5, Math.random() - 0.5 ).mult( r );
+                    d = Physics.body('circle', {
+                        x: pos.get(0) + rnd.get(0),
+                        y: pos.get(1) + rnd.get(1),
+                        vx: this.state.vel.get(0) + (Math.random() - 0.5),
+                        vy: this.state.vel.get(1) + (Math.random() - 0.5),
+                        angularVelocity: (Math.random()-0.5) * 0.06,
+                        mass: mass,
+                        radius: size,
+                        restitution: 0.8
+                    });
+                    d.gameType = 'debris';
+
+                    debris.push( d );
+                }
+
+                setTimeout(function(){
+                    for ( var i = 0, l = debris.length; i < l; ++i ){
+                        world.removeBody( debris[ i ] );
+                    }
+                    debris = undefined;
+                }, 1000);
+
+                world.add( debris );
+                world.removeBody( this );
+                scratch.done();
             }
         };
     });
 
-    for ( var i = 0, l = 40; i < l; ++i ){
+    for ( var i = 0, l = 80; i < l; ++i ){
 
         var ang = 4 * (Math.random() - 0.5) * Math.PI;
         var r = 700 + 100 * Math.random() + i * 10;
@@ -205,7 +332,7 @@ Physics(function( world, Physics ){
             vy: - 0.035 * Math.cos( ang ),
             angularVelocity: (Math.random() - 0.5) * 0.001,
             radius: 50,
-            mass: 50,
+            mass: 30,
             restitution: 0.6
         }));
     }
