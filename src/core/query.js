841828d34b43449e89f8783097a247b37cc1ec72
Jasper <wellcaffeinated@iCoffee.local>
2013-12-28 15:48:25 -0500

    added query building helpers

--- src/core/query.js
+++ src/core/query.js
@@ -1,6 +1,270 @@
 (function (window) {
 
     /**
+     * Helpers
+     */
+    
+    // @TODO edge cases aren't working
+    
+    /*!
+     * Fast indexOf
+     * @param  {Array} arr   The array to search
+     * @param  {Mixed} value The value to find
+     * @return {Number}       The index OR -1
+     */
+    var indexOf = function indexOf(arr, value) {
+        var fr = 0, bk = arr.length;
+        while (fr < bk) {
+            bk--;
+            if (arr[ fr ] === value) {
+                return fr;
+            }
+            if (arr[ bk ] === value) {
+                return bk;
+            }
+            fr++;
+        }
+        return -1;
+    };
+
+    /*!
+     * Get test function to test on sub property
+     * @param  {Function} fn   The test function
+     * @param  {String}   prop The property name to test
+     * @return {Function}        The wrapped function
+     */
+    var wrapRule = function wrapRule( fn, prop ){
+        return function( thing ){
+            return fn( thing[ prop ] );
+        };
+    };
+
+    /*!
+     * Get an equality test function
+     * @param  {Mixed} toMatch The value to match
+     * @param  {String} prop    (optional) The property name to test
+     * @return {Function}         The test function
+     */
+    var $eq = function $eq( toMatch, prop ){
+        return function( thing ){
+            
+            thing = prop ? thing[ prop ] : thing;
+            
+            if ( Physics.util.isArray( thing ) ){
+                // find in array
+                return (indexOf( thing, toMatch ) > -1);
+            }
+
+            // exact match
+            return (thing === toMatch);
+        };
+    };
+
+    /*!
+     * Get a NOT equality test function
+     * @param  {Mixed} toMatch The value to match
+     * @param  {String} prop    (optional) The property name to test
+     * @return {Function}         The test function
+     */
+    var $ne = function $ne( toMatch, prop ){
+        return function( thing ){
+            
+            thing = prop ? thing[ prop ] : thing;
+            
+            if ( Physics.util.isArray( thing ) ){
+                // find in array
+                return (indexOf( thing, toMatch ) === -1);
+            }
+
+            // exact match
+            return (thing !== toMatch);
+        };
+    };
+
+    /*!
+     * Get a test function for matching ANY in array
+     * @param  {Mixed} toMatch The value array to match
+     * @param  {String} prop    (optional) The property name to test
+     * @return {Function}         The test function
+     */
+    var $in = function $in( toMatch, prop ){
+        return function( thing ){
+
+            thing = prop ? thing[ prop ] : thing;
+            
+            var fr = 0
+                ,bk
+                ;
+
+            if ( Physics.util.isArray( thing ) ){
+                bk = thing.length;
+
+                while( fr < bk ){
+                    bk--;
+                    if (
+                        // check front
+                        (indexOf(toMatch, thing[ fr ]) > -1) ||
+                        // check back
+                        (indexOf(toMatch, thing[ bk ]) > -1)
+                    ) {
+                        return true;
+                    }
+                    fr++;
+                }
+                return false;
+            }
+
+            // if thing matches any in array
+            return (indexOf(toMatch, thing) > -1);
+        };
+    };
+
+    /*!
+     * Get a test function for matching NOT ANY in array
+     * @param  {Mixed} toMatch The value array to match
+     * @param  {String} prop    (optional) The property name to test
+     * @return {Function}         The test function
+     */
+    var $nin = function $nin( toMatch, prop ){
+        return function( thing ){
+
+            thing = prop ? thing[ prop ] : thing;
+            
+            var fr = 0
+                ,bk
+                ;
+
+            if ( Physics.util.isArray( thing ) ){
+                bk = thing.length;
+
+                while( fr < bk ){
+                    bk--;
+                    if (
+                        // check front
+                        (indexOf(toMatch, thing[ fr ]) > -1) ||
+                        // check back
+                        (indexOf(toMatch, thing[ bk ]) > -1)
+                    ) {
+                        return false;
+                    }
+                    fr++;
+                }
+                return true;
+            }
+
+            // if thing matches any in array
+            return (indexOf(toMatch, thing) === -1);
+        };
+    };
+
+    /*!
+     * Get an AND test function
+     * @param  {Function} first First function node
+     * @return {Function}       Test function
+     */
+    var $and = function $and( first ){
+        return first.next ? function( thing ){
+            var fn = first;
+            while ( fn ){
+
+                if ( !fn( thing ) ){
+                    return false;
+                }
+                fn = fn.next;
+            }
+            return true;
+        } : first;
+    };
+
+    /*!
+     * Get an OR test function
+     * @param  {Function} first First function node
+     * @return {function}       Test function
+     */
+    var $or = function $or( first ){
+        return first.next ? function( thing ){
+            var fn = first;
+            while ( fn ){
+
+                if ( fn( thing ) ){
+                    return true;
+                }
+                fn = fn.next;
+            }
+            return false;
+        } : first;
+    };
+
+    // operation hash
+    var operations = {
+        // $and and $or are separate
+        $eq: $eq
+        ,$ne: $ne
+        ,$in: $in
+        ,$nin: $nin
+    };
+
+    var build = function build( rules, $op ){
+
+        var op
+            ,l
+            ,rule
+            ,first
+            ,list
+            ,fn
+            ;
+
+        if ( $op ){
+            
+            // parse operation choice
+            if ( $op === '$or' || $op === '$and' ){
+
+                // expect a rules array
+                for ( op = 0, l = rules.length; op < l; ++op ){
+                    
+                    fn = build( rules[ op ] );
+                    // if first hasn't been set yet, set it and start the list there
+                    // otherwise set the next node of the list
+                    list = list ? list.next = fn : first = fn;
+                }
+
+                return ($op === '$or') ? $or( first ) : $and( first );
+            } else if ( op = operations[ $op ] ){
+
+                return op( rules );
+
+            } else {
+                // does not compute...
+                throw 'Unknown query operation: ' + $op;
+            }
+        }
+
+        // loop through rules
+        for ( op in rules ){
+            rule = rules[ op ];
+   
+            if ( op[0] === '$' ){
+                // it's an operation rule
+                fn = build( rule, op );
+                
+            } else if ( typeof rule === 'object' ) {
+                // it's an object so parse subrules
+                fn = wrapRule( build( rule ), op );
+            } else {
+                // simple equality rule
+                fn = $eq( rule, op );
+            }
+
+            // if first hasn't been set yet, set it and start the list there
+            // otherwise set the next node of the list
+            list = list ? list.next = fn : first = fn;
+        }
+
+        // return the rules test
+        return $and( first );
+    };
+
+    /**
      * Query Constructor / Factory
      * @param {Query} base (optional) A query to extend
      * @param {Object} q The query rules
