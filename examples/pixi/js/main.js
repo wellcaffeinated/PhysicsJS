6beb0a817bc99743f0b88628e8284d6dffbdee8c
ngallagher87 <nathan@discoverysoftware.com>
2014-01-26 13:50:08 -0800

    Fixing tabs and removing setInterval for updates in ticker

--- examples/pixi/js/main.js
+++ examples/pixi/js/main.js
@@ -5,88 +5,86 @@
  * Simple "Hello world" example
  */
 Physics(function(world){
-
-  var viewWidth = 500;
-  var viewHeight = 300;
-
-  var renderer = Physics.renderer('pixi', {
-	el: 'viewport',
-	width: viewWidth,
-	height: viewHeight,
-	meta: true, // don't display meta data
-  });
-
-  rigidConstraints = Physics.behavior('rigid-constraint-manager', {
-  	targetLength: 3
-  })
-  
-  // add the renderer
-  world.add( renderer );
-  
-  // Load our spritesheet assets
-  renderer.loadSpritesheets(['img/enemy.json'], loadActors);
-  
-  // render on each step
-  world.subscribe('step', function(){
-	world.render();
-  });
-
-  // bounds of the window
-  var viewportBounds = Physics.aabb(0, 0, viewWidth, viewHeight);
-
-  // constrain objects to these bounds
-  world.add(Physics.behavior('edge-collision-detection', {
-	  aabb: viewportBounds,
-	  restitution: 0.99,
-	  cof: 0.99
-  }));
-  
-  // ensure objects bounce when edge collision is detected
-  world.add( Physics.behavior('body-impulse-response') );
-  // Bounce the comet of the monster
-  world.add( Physics.behavior('body-collision-detection', { checkAll: true }) );
-  
-  world.add( rigidConstraints );
-  // subscribe to ticker to advance the simulation
-  Physics.util.ticker.subscribe(function( time, dt ){
-
-	  world.step( time );
-  });
-  
-  var container = new PIXI.DisplayObjectContainer();
-  // Apply some cool pixi effects here!
-  var displacementTexture = PIXI.Texture.fromImage("img/displacement_map.jpg");
-  var displacementFilter = new PIXI.DisplacementFilter(displacementTexture);
-  
-  var bg = PIXI.Sprite.fromImage("img/displacement_BG.jpg");
-  container.addChild(bg);
-  
-  var overlay = new PIXI.TilingSprite(PIXI.Texture.fromImage("img/zeldaWaves.png"), 630, 410);
-  overlay.alpha = 0.1;
-  
-  setInterval(function() {
-	  overlay.tilePosition.x -= 1;
-	  overlay.tilePosition.y -= 1.5;
-	  displacementFilter.offset.x += 5;
-	  displacementFilter.offset.y += 5;
-  }, 50);
-  
-  renderer.stage.addChild(container);
-  renderer.stage.filters = [displacementFilter];
-  displacementFilter.scale.x = 50;
-  displacementFilter.scale.y = 50;
-
-  // start the ticker
-  Physics.util.ticker.start();
-  
-  function loadActors() {
+	
+	var viewWidth = 500;
+	var viewHeight = 300;
+	
+	var renderer = Physics.renderer('pixi', {
+		el: 'viewport',
+		width: viewWidth,
+		height: viewHeight,
+		meta: true
+	});
+	
+	// add the renderer
+	world.add( renderer );
+	
+	// Load our spritesheet assets
+	// Call loadActors() defined below when assets are loaded
+	renderer.loadSpritesheets(['img/enemy.json'], loadActors);
+	
+	// render on each step
+	world.subscribe('step', function(){
+		world.render();
+	});
+	
+	// bounds of the window
+	var viewportBounds = Physics.aabb(0, 0, viewWidth, viewHeight);
+	
+	// constrain objects to these bounds
+	world.add(Physics.behavior('edge-collision-detection', {
+		aabb: viewportBounds,
+		restitution: 0.99,
+		cof: 0.99
+	}));
+	
+	// ensure objects bounce when edge collision is detected
+	world.add( Physics.behavior('body-impulse-response') );
+	// Bounce the comet of the monster
+	world.add( Physics.behavior('body-collision-detection', { checkAll: true }) );
+	
+	// subscribe to ticker to advance the simulation
+	Physics.util.ticker.subscribe(function( time, dt ){
+	
+		world.step( time );
+		
+		// Move our displacement filter and overlay here
+		overlay.tilePosition.x -= 0.5;
+		overlay.tilePosition.y -= 1;
+		displacementFilter.offset.x += 2;
+		displacementFilter.offset.y += 2;
+	});
+	
+	/**
+	Lets add some cool PIXI stuff here, like some filters and backgrounds
+	*/
+	var container = new PIXI.DisplayObjectContainer();
+	// Apply some cool pixi effects here!
+	var displacementTexture = PIXI.Texture.fromImage("img/displacement_map.jpg");
+	var displacementFilter = new PIXI.DisplacementFilter(displacementTexture);
+	displacementFilter.scale.x = 50;
+	displacementFilter.scale.y = 50;
+	
+	var bg = PIXI.Sprite.fromImage("img/displacement_BG.jpg");
+	container.addChild(bg);
+	
+	var overlay = new PIXI.TilingSprite(PIXI.Texture.fromImage("img/zeldaWaves.png"), 630, 410);
+	overlay.alpha = 0.1;
+	
+	renderer.stage.addChild(container);
+	renderer.stage.filters = [displacementFilter];
+	
+	// start the ticker
+	Physics.util.ticker.start();
+	
+	function loadActors() {
 		// Example of a sprite
 		var comet = Physics.body('circle', {
-		  	x: 50, // x-coordinate
-		  	y: 70, // y-coordinate
-		  	vx: 0.2, // velocity in x-direction
-		  	vy: 0.11, // velocity in y-direction,
-		  	radius: 20
+			x: 50, // x-coordinate
+			y: 70, // y-coordinate
+			vx: 0.2, // velocity in x-direction
+			vy: 0.11, // velocity in y-direction,
+			radius: 20
 		});
 		comet.view = renderer.createDisplay('sprite', {
 			texture: 'img/comet.png',
@@ -105,26 +103,26 @@ Physics(function(world){
 		// Example of a movie clip
 		var enemy = Physics.body('convex-polygon', {
 			vertices: square,
-		  	x: 50, // x-coordinate
-		  	y: 100, // y-coordinate
-		  	vx: 0.4, // velocity in x-direction
-		  	vy: 0.01, // velocity in y-direction,
-		  	mass: 1,
-		  	restitution: 0.5,
-		  	cof: 1
+			x: 50, // x-coordinate
+			y: 100, // y-coordinate
+			vx: 0.4, // velocity in x-direction
+			vy: 0.01, // velocity in y-direction,
+			mass: 1,
+			restitution: 0.5,
+			cof: 1
 		});
 		enemy.view = renderer.createDisplay('movieclip', {
 			// Define the animations frames here
 			frames: [
-			  	'enemy-walk-1.png', 'enemy-walk-2.png',
-			  	'enemy-walk-3.png', 'enemy-walk-4.png',
-			  	'enemy-walk-5.png', 'enemy-walk-6.png',
-			  	'enemy-walk-5.png', 'enemy-walk-4.png',
-			  	'enemy-walk-3.png', 'enemy-walk-2.png'
+				'enemy-walk-1.png', 'enemy-walk-2.png',
+				'enemy-walk-3.png', 'enemy-walk-4.png',
+				'enemy-walk-5.png', 'enemy-walk-6.png',
+				'enemy-walk-5.png', 'enemy-walk-4.png',
+				'enemy-walk-3.png', 'enemy-walk-2.png'
 			],
 			anchor: {
-			  	x: 0.5,
-			  	y: 0.5
+				x: 0.5,
+				y: 0.5
 			}
 		});
 		enemy.view.animationSpeed = 0.2;
@@ -162,6 +160,6 @@ Physics(function(world){
 		world.add( poly );
 		
 		renderer.stage.addChild(overlay);
-    }
+	}
 
 });
