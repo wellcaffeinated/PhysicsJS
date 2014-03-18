6ad9736858a33189f3b54ab11706c0f0f5ff22e5
Jasper Palfree <well.caffeinated@gmail.com>
2014-03-18 11:50:22 -0400

    doc modifications

--- docs/js/page.js
+++
@@ -1,8 +0,0 @@
-  var headerHeight = 64;
-
-  $(document).foundation();
-
-  $(function(){
-    $('pre code').addClass('language-javascript');
-  //   $('#nav > ul > li > a[href$="'+location.pathname.substring(location.pathname.lastIndexOf('/')+1, location.pathname.length)+'"]').parent().addClass('active');
-  })
