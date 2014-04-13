754d842f314a2ed7107452156fd142c2f19c66ee
Jasper Palfree <well.caffeinated@gmail.com>
2014-04-13 14:36:23 -0400

    feature: added layers to canvas renderer

--- src/renderers/canvas.js
+++ src/renderers/canvas.js
@@ -19,6 +19,14 @@ Physics.renderer('canvas', function( proto ){
             }
             return el;
         }
+        ,colors = {
+            white: '#fff'
+            ,violet: '#542437'
+            ,blue: '#53777A'
+            ,gold: '#ECD078'
+            ,orange: '#D95B43'
+            ,pink: '#C02942'
+        }
         ;
 
     var defaults = {
@@ -30,23 +38,21 @@ Physics.renderer('canvas', function( proto ){
         // default styles of drawn objects
         styles: {
 
-            'point' : 'rgba(80, 50, 100, 0.7)',
-
             'circle' : {
-                strokeStyle: 'rgba(70, 50, 100, 0.7)',
+                strokeStyle: colors.blue,
                 lineWidth: 1,
-                fillStyle: 'rgba(44, 105, 44, 0.7)',
-                angleIndicator: 'rgba(69, 51, 78, 0.7)'
+                fillStyle: colors.blue,
+                angleIndicator: colors.white
             },
 
             'convex-polygon' : {
-                strokeStyle: 'rgba(80, 50, 100, 0.7)',
+                strokeStyle: colors.violet,
                 lineWidth: 1,
-                fillStyle: 'rgba(114, 105, 124, 0.7)',
-                angleIndicator: 'rgba(69, 51, 78, 0.7)'
+                fillStyle: colors.violet,
+                angleIndicator: colors.white
             }
         },
-        offset: {x: 0, y: 0}
+        offset: { x: 0, y: 0 }
     };
 
     // deep copy callback to extend deeper into options
@@ -82,7 +88,7 @@ Physics.renderer('canvas', function( proto ){
             // hidden canvas
             this.hiddenCanvas = document.createElement('canvas');
             this.hiddenCanvas.width = this.hiddenCanvas.height = 100;
-            
+
             if (!this.hiddenCanvas.getContext){
                 throw "Canvas not supported";
             }
@@ -91,7 +97,7 @@ Physics.renderer('canvas', function( proto ){
 
             // actual viewport
             var viewport = this.el;
-            if (viewport.nodeName.toUpperCase() !== "CANVAS"){
+            if (viewport.nodeName.toUpperCase() !== 'CANVAS'){
 
                 viewport = document.createElement('canvas');
                 this.el.appendChild( viewport );
@@ -101,10 +107,7 @@ Physics.renderer('canvas', function( proto ){
                 this.el = viewport;
             }
 
-            viewport.width = this.options.width;
-            viewport.height = this.options.height;
-
-            this.ctx = viewport.getContext("2d");
+            this.ctx = viewport.getContext('2d');
 
             this.els = {};
 
@@ -121,6 +124,174 @@ Physics.renderer('canvas', function( proto ){
 
                 viewport.parentNode.insertBefore(stats, viewport);
             }
+
+            this._layers = {};
+            this.addLayer( 'main', this.el );
+            this.resize( this.options.width, this.options.height );
+        },
+
+        layer: function( name ){
+
+            if ( name in this._layers ){
+                return this._layers[ name ];
+            }
+
+            return null;
+        },
+
+        addLayer: function( id, el, opts ){
+
+            var self = this
+                ,bodies = []
+                ,styles = Physics.util.extend({}, this.options.styles)
+                ,layer = {
+                    id: id
+                    ,el: el || document.createElement('canvas')
+                    ,options: Physics.util.options({
+                        width: this.el.width
+                        ,height: this.el.height
+                        ,manual: false
+                        ,autoResize: true
+                        ,follow: null
+                        ,scale: 1
+                        ,zIndex: 1
+                    })( opts )
+                }
+                ;
+
+            if ( id in this._layers ){
+                throw 'Layer "' + id + '" already added.';
+            }
+
+            this.el.parentNode.insertBefore( layer.el, this.el );
+            layer.el.style.position = 'absolute';
+            layer.el.style.zIndex = layer.options.zIndex;
+            layer.el.className += ' pjs-layer-' + layer.id;
+            layer.ctx = layer.el.getContext('2d');
+            layer.ctx.scale( 1, 1 );
+            layer.el.width = layer.options.width;
+            layer.el.height = layer.options.height;
+
+            layer.bodies = bodies;
+
+            layer.reset = function( arr ){
+
+                bodies = arr || [];
+            };
+
+            layer.addToStack = function( thing ){
+
+                if ( Physics.util.isArray( thing ) ){
+                    bodies.push.apply( bodies, thing );
+                } else {
+                    bodies.push( thing );
+                }
+                return layer;
+            };
+
+            layer.removeFromStack = function( thing ){
+
+                var i, l;
+
+                if ( Physics.util.isArray( thing ) ){
+                    for ( i = 0, l = thing.length; i < l; ++i ){
+                        layer.removeFromStack(thing[ i ]);
+                    }
+                } else {
+                    i = Physics.util.indexOf( bodies, thing );
+                    if ( i > -1 ){
+                        bodies.splice( i, 1 );
+                    }
+                }
+                return layer;
+            };
+
+            layer.render = function( clear ){
+
+                var body
+                    ,scratch = Physics.scratchpad()
+                    ,offset = scratch.vector().set(0, 0)
+                    ,scale = layer.options.scale
+                    ,view
+                    ,i
+                    ,l = bodies.length
+                    ,stack = l ? bodies : self._world._bodies
+                    ;
+
+                if ( layer.options.manual ){
+                    scratch.done();
+                    return layer;
+                }
+
+                if ( layer.options.offset ){
+                    if ( layer.options.offset === 'center' ){
+                        offset.add( layer.el.width * 0.5, layer.el.height * 0.5 ).mult( 1/scale );
+                    } else {
+                        offset.vadd( layer.options.offset ).mult( 1/scale );
+                    }
+                }
+
+                if ( layer.options.follow ){
+                    offset.vsub( layer.options.follow.state.pos );
+                }
+
+                if ( clear !== false ){
+                    layer.ctx.clearRect(0, 0, layer.el.width, layer.el.height);
+                }
+
+                if ( scale !== 1 ){
+                    layer.ctx.save();
+                    layer.ctx.scale( scale, scale );
+                }
+
+                for ( i = 0, l = stack.length; i < l; ++i ){
+
+                    body = stack[ i ];
+                    if ( !body.hidden ){
+                        view = body.view || ( body.view = self.createView(body.geometry, body.styles || styles[ body.geometry.name ]) );
+                        self.drawBody( body, body.view, layer.ctx, offset );
+                    }
+                }
+
+                if ( scale !== 1 ){
+                    layer.ctx.restore();
+                }
+
+                scratch.done();
+                return layer;
+            };
+
+            // remember layer
+            this._layers[ id ] = layer;
+
+            return layer;
+        },
+
+        removeLayer: function( idOrLayer ){
+
+            var id = idOrLayer.id ? idOrLayer.id : idOrLayer
+                ,el = this._layers[ id ].el
+                ;
+
+            if ( el !== this.el ){
+                el.parentNode.removeChild( el );
+            }
+            delete this._layers[ id ];
+            return this;
+        },
+
+        resize: function( width, height ){
+
+            var layer;
+
+            for ( var id in this._layers ){
+
+                layer = this._layers[ id ];
+                if ( layer.options.autoResize ){
+                    layer.el.width = width;
+                    layer.el.height = height;
+                }
+            }
         },
 
         /**
@@ -134,9 +305,8 @@ Physics.renderer('canvas', function( proto ){
 
             if ( Physics.util.isObject(styles) ){
 
-                ctx.lineWidth = styles.lineWidth;
-                ctx.strokeStyle = styles.lineWidth ? styles.strokeStyle : 'rgba(0,0,0,0)';
-                ctx.fillStyle = styles.fillStyle;
+                styles.strokeStyle = styles.lineWidth ? styles.strokeStyle : 'rgba(0,0,0,0)';
+                Physics.util.extend(ctx, styles);
 
             } else {
 
@@ -188,14 +358,14 @@ Physics.renderer('canvas', function( proto ){
             ctx.moveTo(x, y);
 
             for ( var i = 1; i < l; ++i ){
-                
+
                 vert = verts[ i ];
                 x = vert.x;
                 y = vert.y;
                 ctx.lineTo(x, y);
             }
 
-            if (l > 2){
+            if ( l > 2 ){
                 ctx.closePath();
             }
 
@@ -251,9 +421,9 @@ Physics.renderer('canvas', function( proto ){
 
             x = to.x;
             y = to.y;
-            
+
             ctx.lineTo(x, y);
-            
+
             ctx.stroke();
             ctx.fill();
         },
@@ -277,7 +447,7 @@ Physics.renderer('canvas', function( proto ){
                 ,name = geometry.name
                 ;
 
-            styles = styles || this.options.styles[ name ];
+            styles = styles || this.options.styles[ name ] || {};
 
             // must want an image
             if ( styles.src ){
@@ -294,7 +464,7 @@ Physics.renderer('canvas', function( proto ){
 
             x += styles.lineWidth | 0;
             y += styles.lineWidth | 0;
-            
+
             // clear
             hiddenCanvas.width = 2 * hw + 2 + (2 * styles.lineWidth|0);
             hiddenCanvas.height = 2 * hh + 2 + (2 * styles.lineWidth|0);
@@ -340,17 +510,6 @@ Physics.renderer('canvas', function( proto ){
         },
 
         /**
-         * Callback to be run before rendering
-         * @private
-         * @return {void}
-         */
-        beforeRender: function(){
-
-            // clear canvas
-            this.ctx.clearRect(0, 0, this.el.width, this.el.height);
-        },
-
-        /**
          * Draw a body to canvas
          * @param  {Body} body The body to draw
          * @param  {Image} view The view for that body
@@ -375,7 +534,7 @@ Physics.renderer('canvas', function( proto ){
                 aabb = body.aabb();
                 // draw bounding boxes
                 this.drawRect( aabb.x, aabb.y, 2 * aabb.hw, 2 * aabb.hh, 'rgba(0, 0, 255, 0.3)' );
-                
+
                 // draw also paths
                 body._debugView = body._debugView || this.createView(body.geometry, 'rgba(255, 0, 0, 0.5)');
                 ctx.save();
@@ -384,6 +543,30 @@ Physics.renderer('canvas', function( proto ){
                 ctx.drawImage(body._debugView, -body._debugView.width * 0.5, -body._debugView.height * 0.5);
                 ctx.restore();
             }
+        },
+
+        render: function( bodies, meta ){
+
+            var body
+                ,view
+                ,pos
+                ;
+
+            this._world.emit('beforeRender', {
+                renderer: this,
+                meta: meta
+            });
+
+            if ( this.options.meta ) {
+                this.drawMeta( meta );
+            }
+
+            for ( var id in this._layers ){
+
+                this._layers[ id ].render();
+            }
+
+            return this;
         }
     };
 });
